7c7f8b876015d4d8f5af64f5c7dda9f83e49f27a
Giuseppe Vetri <giuseppe@codingpizza.com>
2021-11-20 20:01:06 +0100

    Updated versions. Implemented Kts

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+buildscript {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
+        classpath("com.android.tools.build:gradle:7.0.3")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.38.1")
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
