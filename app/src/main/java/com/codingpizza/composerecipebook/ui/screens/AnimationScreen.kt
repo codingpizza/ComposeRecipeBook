72a42b2fe157a1ba9fa6a3ce04867f470b3ba470
Giuseppe Vetri <giuseppe@codingpizza.com>
2022-01-13 22:04:16 +0100

    Implemented CrossFade animation

--- app/src/main/java/com/codingpizza/composerecipebook/ui/screens/AnimationScreen.kt
+++ app/src/main/java/com/codingpizza/composerecipebook/ui/screens/AnimationScreen.kt
@@ -1,12 +1,18 @@
 package com.codingpizza.composerecipebook.ui.screens
 
+import androidx.compose.animation.Crossfade
 import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material.icons.rounded.Delete
+import androidx.compose.material.icons.rounded.Lock
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -49,9 +55,38 @@ fun AnimatedTextComposable() {
     }
 }
 
+@Composable
+fun CrossFadeComposable() {
+    var currentPage by remember { mutableStateOf("Add") }
+    Crossfade(targetState = currentPage) { screen ->
+        when (screen) {
+            "Add" -> {
+                Icon(
+                    Icons.Rounded.Add,
+                    contentDescription = "Add item",
+                    modifier = Modifier.padding(start = 16.dp, top = 8.dp, end = 16.dp, bottom = 8.dp)
+                )
+            }
+            "Delete" -> {
+                Icon(
+                    Icons.Rounded.Delete,
+                    contentDescription = "Delete item",
+                    modifier = Modifier.padding(start = 16.dp, top = 8.dp, end = 16.dp, bottom = 8.dp)
+                )
+            }
+        }
+    }
+    Button(onClick = {
+        currentPage = (if (currentPage == "Add") "Delete" else "Add")
+    }, modifier = Modifier.padding(start = 16.dp, top = 8.dp)) {
+        Text(text = "Update animation", color = MaterialTheme.colors.onPrimary)
+    }
+}
+
 fun getRandomText(): String = LoremIpsum().values.first().split(" ").shuffled().firstOrNull() ?: ""
 
 @Composable
 private fun composableList(): List<ComposableExample> = listOf(
-    ComposableExample(title = "Animated TextField", composable = { AnimatedTextComposable() }),
+    ComposableExample(title = "Animated Content size", composable = { AnimatedTextComposable() }),
+    ComposableExample(title = "Crossfade Animation", composable = { CrossFadeComposable() }),
 )
