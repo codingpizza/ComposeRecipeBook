3a0b1b97eb7bf5bd73606067100c4a7de267b153
Giuseppe Vetri <giuseppe@codingpizza.com>
2022-02-10 21:50:56 +0100

    Updated to Compose 1.1.0 and latest AGP, Hilt.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,7 +45,7 @@ android {
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.0.5"
+        kotlinCompilerExtensionVersion = "1.1.0"
     }
 
     packagingOptions {
@@ -59,19 +59,19 @@ android {
 }
 
 dependencies {
-    val compose_version = "1.0.5"
+    val compose_version = "1.1.0"
     implementation("androidx.core:core-ktx:1.7.0")
     implementation("androidx.compose.ui:ui:$compose_version")
     implementation("androidx.compose.ui:ui-tooling-preview:$compose_version")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0")
     implementation("androidx.activity:activity-compose:1.4.0")
-    implementation("androidx.compose.material:material:1.0.5")
+    implementation("androidx.compose.material:material:$compose_version")
     implementation("io.coil-kt:coil-compose:1.4.0")
 
 
     //Hilt
-    implementation ("com.google.dagger:hilt-android:2.38.1")
-    kapt ("com.google.dagger:hilt-compiler:2.38.1")
+    implementation ("com.google.dagger:hilt-android:2.40.5")
+    kapt ("com.google.dagger:hilt-compiler:2.40.5")
     implementation ("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03")
     implementation ("androidx.hilt:hilt-navigation-compose:1.0.0")
     kapt ("androidx.hilt:hilt-compiler:1.0.0")
--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,9 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
-        classpath("com.android.tools.build:gradle:7.1.0")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.38.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
+        classpath("com.android.tools.build:gradle:7.1.1")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.40.5")
     }
 }
 
